928dd4d5319b46d03ba7a5d3e04f726365db21b6
EdgarVerdi <edgar.verdi@gmail.com>
2024-04-02 20:18:47 +0100

    objectified the trident for better code reading and easy merging with other solvers

--- tkinter_tests/trident_test3.py
+++ tkinter_tests/trident_test3.py
@@ -1,3 +1,4 @@
+import re
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -5,6 +6,20 @@ from matplotlib.backends.backend_tkagg import (
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import numpy as np
+
+
+def excel_col(col):
+    """
+    Covert 0-relative column number to excel-style column label.
+    inspired by: https://stackoverflow.com/questions/19153462/get-excel-style-column-names-from-column-number
+    """
+    col+=1
+    t = ''
+    while col != 0:
+        col, rem = divmod(col-1,26)
+        t = chr(rem+ord('A')) + t
+    return t
+
 
 window = tk.Tk()
 window.geometry("600x800")
@@ -32,13 +47,22 @@ def genNewTable():
 
     # less rows
     for i in range(numberOfRowsInUse, oldNumberOfRowsInUse):
-        for j in range(3):
+        for j in range(0, 4):
             text = cells[(i, j)]
             text.grid_forget()
 
     # more rows
     for i in range(oldNumberOfRowsInUse, numberOfRowsInUse):
-        for j in range(3):
+        # putting the Text Cell
+        text = cells.get((i, 0), None)
+        if text is None:
+            text = tk.Text(tableFrame, width=16, height=1, bg="#A0A0A0")
+            text.insert(tk.INSERT, excel_col(i))
+            cells[(i, 0)] = text
+        text.grid(row=i + 1, column=0)
+
+        # putting the Value Cells
+        for j in range(1, 4):
             text = cells.get((i, j), None)
             if text is None:
                 text = tk.Text(tableFrame, width=16, height=1)
@@ -47,14 +71,27 @@ def genNewTable():
             text.grid(row=i + 1, column=j)
 
 
+def putFromClipBoard():
+    clip = window.clipboard_get().strip("\n")
+    line = [[float(f) for f in re.findall("(\d+\.\d|\d+)", l)] for l in clip.split("\n")]
+    for i, l in enumerate(line):
+        for j, f in zip(range(1, 4), l):
+            text = cells[(i, j)]
+            text.delete(1.0, tk.END)
+            text.insert(tk.END, f)
+
+
 createTableButton = tk.Button(rowsQuestionFrame, text="Create Table", command=genNewTable)
 createTableButton.grid(row=1, column=0)
+
+importTableButton = tk.Button(rowsQuestionFrame, text="Import Table From Clipboard", command=putFromClipBoard)
+importTableButton.grid(row=1, column=1)
 
 
 def getVariableEquations():
     # sorry for the one-liner but it just felt way more efficient than append xD
     try:
-        l = [tuple([float(cells[(i, j)].get("1.0", 'end-1c')) for j in range(3)]) for i in range(numberOfRowsInUse)]
+        l = [tuple([float(cells[(i, j)].get("1.0", 'end-1c')) for j in range(1, 4)]) for i in range(numberOfRowsInUse)]
         return [np.asarray([A - C, B - C, C]) for A, B, C in l]
     except ValueError:
         raise ValueError  # TODO better error here
@@ -66,12 +103,13 @@ def plotFunc():
     t2 = np.arange(1, 0, -.01)
     # fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
     sub = fig.add_subplot(111)
-    sub.plot(t, t2)
+    sub.plot(t, t2, label="max", color="black")
 
     variableEq = getVariableEquations()
-    for y, x, c in [a - b for idx, a in enumerate(variableEq) for b in variableEq[idx + 1:]]:
+    cell_names = [cells[(i, 0)].get('1.0', 'end-1c') for i in range(numberOfRowsInUse)]
+    for (y, x, c), i1, i2 in [(a - b, idx, idy) for idx, a in enumerate(variableEq) for idy, b in enumerate(variableEq[idx + 1:], idx + 1)]:
         if y != 0:
-            sub.plot(t, -(x * t + c) / y)
+            sub.plot(t, -(x * t + c) / y, label=f"{cell_names[i1]}_{cell_names[i2]}")
 
     sub.set_xlim([0, 1])
     sub.set_ylim([0, 1])
@@ -80,6 +118,7 @@ def plotFunc():
         widget.destroy()  # clean previous canvas
 
     canvas = FigureCanvasTkAgg(fig, master=canvasFrame)  # A tk.DrawingArea.
+    sub.legend()
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
